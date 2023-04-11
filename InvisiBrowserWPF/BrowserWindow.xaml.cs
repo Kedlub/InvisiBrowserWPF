22fd90f9bc83e91a4eda450101366968ab6fe16b
Kedlub <jakubkyzr@gmail.com>
2023-04-11 19:52:47 +0200

    Initial commit

---
+++ InvisiBrowserWPF/BrowserWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interop;
+using MicaWPF.Controls;
+
+namespace InvisiBrowserWPF;
+
+public partial class BrowserWindow : Window
+{
+    public const int WM_NCLBUTTONDOWN = 0xA1;
+    public const int HT_CAPTION = 0x2;
+
+    [DllImport("user32.dll")]
+    public static extern int SendMessage(IntPtr hWnd, int Msg, 
+        int wParam, int lParam);
+    [DllImportAttribute("user32.dll")]
+    public static extern bool ReleaseCapture();
+    
+    
+    private string _url = "";
+    public BrowserWindow()
+    {
+        InitializeComponent();
+    }
+    
+    public BrowserWindow(string url): this()
+    {
+        if (!url.Contains("://"))
+        {
+            url = "https://" + url;
+        }
+        _url = url;
+        Browser.Source = new System.Uri(_url);
+    }
+
+    private async void BrowserWindow_OnLoaded(object sender, RoutedEventArgs e)
+    {
+    }
+
+    private void BrowserWindow_OnClosed(object? sender, EventArgs e)
+    {
+        Application.Current.Shutdown();
+    }
+
+    private void BrowserWindow_OnMouseMove(object sender, MouseEventArgs e)
+    {
+    }
+    
+    public void move_window(object sender, MouseButtonEventArgs e) {
+        ReleaseCapture();
+        SendMessage(new WindowInteropHelper(this).Handle, 
+            WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+    }
+    
+}
---
+++ InvisiBrowserWPF/PromptWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using MicaWPF.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace InvisiBrowserWPF
+{
+    /// <summary>
+    /// Interakční logika pro PromptWindow.xaml
+    /// </summary>
+    public partial class PromptWindow : MicaWindow
+    {
+        public PromptWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            BrowserWindow browserWindow = new BrowserWindow(UrlBox.Text);
+            browserWindow.Show();
+            Hide();
+        }
+    }
+}
